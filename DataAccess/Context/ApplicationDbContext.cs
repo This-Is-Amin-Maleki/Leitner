3d8a4adaa2c1a49be3684509fc0a521a6ea70ffe
Amin Maleki <AminMaleki@Live.com>
2024-04-15 22:49:19 +0330

    Add DbContext on DataAccess

---
+++ DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Caching.Memory;
+using Models;
+using Shared;
+
+namespace DataAccess.Context
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<UserCollection> UserCollections { get; set; }
+
+
+        public int CleanSaveChanges()
+        {
+            int result = base.SaveChanges();
+            return result;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Database = Leitner;Data Source=ARTEMISIA;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
