082b003c017a447a192f0b6518ad97ce607c09a5
Amin Maleki <AminMaleki@Live.com>
2024-04-16 01:12:00 +0330

    Add Group to Model Layer

---
+++ Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public record Group
+    {
+        public long Id { get; set; }
+        public ICollection<Card>? Cards { get; set; }
+        public ICollection<Slot>? Slots { get; set; }
+    }
+}
