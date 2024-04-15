7308b22a99620fb1f0b257e8dfd86544cc2da2f6
Amin Maleki <AminMaleki@Live.com>
2024-04-15 18:17:30 +0330

    Add card to Model Layer

---
+++ Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public record Card
+    {
+        public long Id { get; set; }
+        public String Ask { get; set; }
+        public String Answer { get; set; }
+        public String? Description { get; set; }
+        public bool HasMp3 { get; set; }
+    }
+}
