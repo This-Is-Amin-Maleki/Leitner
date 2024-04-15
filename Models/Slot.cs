aeed384124396a6dadae982821ecd25cc1b841ac
Amin Maleki <AminMaleki@Live.com>
2024-04-16 01:11:43 +0330

    Add Slot to Model Layer

---
+++ Models/Slot.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public record Slot
+    {
+        public long Id { get; set; }
+        public ICollection<UserCollection>? UserCollection { get; set; }
+    }
+}
