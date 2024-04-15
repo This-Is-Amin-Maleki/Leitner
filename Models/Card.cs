2d030714605b693afb2578ee29a6538fb05d8127
Amin Maleki <AminMaleki@Live.com>
2024-04-15 18:03:22 +0330

    add Card to Model

---
+++ Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Card
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String? Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
