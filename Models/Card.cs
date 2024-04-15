d13009d1660208b6b1d681885cd926c51b4814b3
Amin Maleki <AminMaleki@Live.com>
2024-04-15 18:10:30 +0330

    add Status to Card in Model Layer

--- Models/Card.cs
+++ Models/Card.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,6 @@ namespace Models
         public String Name { get; set; }
         public String? Description { get; set; }
         public DateTime Date { get; set; }
+        public CardStatus Status { get; set; }
     }
 }
