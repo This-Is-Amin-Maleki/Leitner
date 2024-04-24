655496e34b31a85dd6bd13271c6739722f22d5bb
Amin Maleki <AminMaleki@Live.com>
2024-04-24 20:06:05 +0330

    Add ICollection Service to Service Layer

---
+++ Services/Interfaces/ICollectionService.cs
@@ -0,0 +1,20 @@
+﻿using Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ICollectionService
+    {
+        Task AddCollectionAsync(CollectionDTO collectionDTO);
+        Task DeleteCollectionAsync(CollectionDTO collectionDTO);
+        Task EditCollectionAsync(CollectionDTO collectionDTO);
+        Task<CollectionDTO> GetCollectionAsync(long id);
+        Task<List<CollectionDTO>> GetCollectionsAsync();
+        Task<CollectionDTO> ReadCollectionAsync(long id);
+        Task<List<CollectionDTO>> ReadCollectionsAsync();
+    }
+}
