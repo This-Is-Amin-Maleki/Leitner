8e67c2a10fa0c37f1ba3d8919b0db6c1cb5b0153
Amin Maleki <AminMaleki@Live.com>
2024-04-24 20:05:29 +0330

    Add Collection View Model to Model Layer

---
+++ Models/ViewModels/CollectionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public record CollectionViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public CollectionStatus Status { get; set; }
+        public long UserId { get; set; }
+
+        public long CardsQ { get; set; }
+        public long BoxCount { get; set; }
+    }
+}
