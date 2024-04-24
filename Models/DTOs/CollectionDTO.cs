076fe5d18dd1ead8e955349a4dd97cbda08f1ca1
Amin Maleki <AminMaleki@Live.com>
2024-04-24 20:04:40 +0330

    add Collection DTO to Model Layer

---
+++ Models/DTOs/CollectionDTO.cs
@@ -0,0 +1,25 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs
+{
+    public record CollectionDTO
+    {
+        public long? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public DateTime PublishedDate { get; set; }
+        [Required]
+        public CollectionStatus Status { get; set; }
+
+        public long UserId { get; set; }
+    }
+}
