bdb056c1a72b9a53e78e0a090195b6fa72f0c2d8
Amin Maleki <AminMaleki@Live.com>
2024-04-24 20:06:19 +0330

    Add  Collection Service to Service Layer

---
+++ Services/Services/CollectionService.cs
@@ -0,0 +1,135 @@
+﻿using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using Models.DTOs;
+using Services.Interfaces;
+
+namespace Services.Services
+{
+    public class CollectionService : ICollectionService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CollectionService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<CollectionDTO>> ReadCollectionsAsync()
+        {//use auto mapper
+            return await _dbContext.Collections
+                .AsNoTracking()
+                .Select(x => new CollectionDTO()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UserId = x.UserId,
+                    Description = x.Description,
+                    PublishedDate = x.PublishedDate,
+                    Status = x.Status,
+                })
+                .ToListAsync();
+        }
+        public async Task<List<CollectionDTO>> GetCollectionsAsync()
+        {//use auto mapper
+            return await _dbContext.Collections
+                .Select(x => new CollectionDTO()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UserId = x.UserId,
+                    Description = x.Description,
+                    PublishedDate = x.PublishedDate,
+                    Status = x.Status,
+                })
+                .ToListAsync();
+        }
+#warning check performance
+        public async Task<CollectionDTO> ReadCollectionAsync(long id)
+        {
+            var collection = await _dbContext.Collections
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return collection is null ?
+                CreateEmptyCollectionDTO() :
+                MapCollectionToDTO(collection);
+        }
+#warning check performance
+        public async Task<CollectionDTO> GetCollectionAsync(long id)
+        {
+            var collection = await _dbContext.Collections
+                .FindAsync(id);
+
+            return collection is null ?
+                CreateEmptyCollectionDTO() :
+                MapCollectionToDTO(collection);
+        }
+        public async Task AddCollectionAsync(CollectionDTO collectionDTO)
+        {
+            var collection = MapDTOToCollection(collectionDTO);
+            await _dbContext.Collections.AddAsync(collection);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task EditCollectionAsync(CollectionDTO collectionDTO)
+        {
+            var collection = MapDTOToCollection(collectionDTO);
+
+            var hasCollection = await _dbContext.Collections
+                .AnyAsync(x => x.Id == collectionDTO.Id);
+
+            if (hasCollection is false)
+            {
+                throw new Exception("Not Found");
+            }
+            await _dbContext.Collections.AddAsync(collection);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task DeleteCollectionAsync(CollectionDTO collectionDTO)
+        {
+            var collection = MapDTOToCollection(collectionDTO);
+
+            var hasCollection = await _dbContext.Collections
+                .AnyAsync(x => x.Id == collectionDTO.Id);
+
+            if (hasCollection is false)
+            {
+                throw new Exception("Not Found");
+            }
+            _dbContext.Collections.Remove(collection);
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+        ////////////////////////////////////////////////////////
+        private CollectionDTO CreateEmptyCollectionDTO()
+        {
+            return new CollectionDTO();
+        }
+
+        private CollectionDTO MapCollectionToDTO(Collection collection)
+        {
+            return new CollectionDTO()
+            {
+                Id = collection.Id,
+                Description = collection.Description,
+                Name = collection.Name,
+                PublishedDate = collection.PublishedDate,
+                Status = collection.Status,
+                UserId = collection.UserId
+            };
+        }
+        private Collection MapDTOToCollection(CollectionDTO collectionDTO)
+        {
+            return new Collection()
+            {
+                Id = collectionDTO.Id ?? 0,
+                Description = collectionDTO.Description,
+                Name = collectionDTO.Name,
+                PublishedDate = DateTime.Now,
+                Status = collectionDTO.Status,
+                UserId = collectionDTO.UserId,
+            };
+        }
+    }
+}
