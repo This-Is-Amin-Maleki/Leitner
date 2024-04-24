e323760055f525c80ca80ef547b56b901453fe19
Amin Maleki <AminMaleki@Live.com>
2024-04-24 20:18:39 +0330

    Add Collection Controller to View

---
+++ View/Controllers/CollectionController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Models.DTOs;
+using Services.Services;
+using Shared;
+
+namespace View.Controllers
+{
+    public class CollectionController: Controller
+    {
+        CollectionService _collectionService;
+        public CollectionController(CollectionService collectionService, RawDbService raw)
+        {
+            _collectionService = collectionService;
+        }
+
+    }
+}
