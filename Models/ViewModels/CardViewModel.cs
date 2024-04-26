617efafc3f74ec220d486d17b6e0d20875f77ec4
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:42:08 +0330

    Create CardViewModel.cs

---
+++ Models/ViewModels/CardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Models.Entities;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public record CardViewModel
+    {
+        public long Id { get; set; }
+        public string Ask { get; set; }
+        public string Answer { get; set; }
+        public string Description { get; set; }
+        public bool HasMp3 { get; set; }
+        public CardCollectionViewModel Collection { get; set; }
+    }
+}
