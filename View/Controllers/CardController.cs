93c1d137c03804676687db64132928451b2d66e3
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:54:45 +0330

    Create CardController.cs

---
+++ View/Controllers/CardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels;
+using Services.Services;
+using System.Collections;
+
+namespace View.Controllers
+{
+    public class CardController: Controller
+    {
+        CardService _cardService;
+        public CardController(CardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+    }
+}
