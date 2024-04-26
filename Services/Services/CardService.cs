2076a4e90c464e624d7a871123d4a9d46297ede0
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:44:32 +0330

    Add ReadCardsAsync to CardService

--- Services/Services/CardService.cs
+++ Services/Services/CardService.cs
@@ -19,5 +19,41 @@ namespace Services.Services
             _dbContext = dbContext;
         }
 
+        public async Task<(List<CardViewModel>, string)> ReadCardsAsync(long collectionId)
+        {//use auto mapper
+            string collectionName = "";
+            List<CardViewModel> list = await _dbContext.Cards
+                .AsNoTracking()
+                .Include(x => x.Collection)
+                .Where(x => x.CollectionId == collectionId)
+                .Select(x => new CardViewModel()
+                {
+                    Id = x.Id,
+                    Ask = x.Ask,
+                    Answer = x.Answer,
+                    Description = x.Description,
+                    HasMp3 = x.HasMp3,
+                    Collection = new CardCollectionViewModel()
+                    {
+                        Id = x.Collection.Id,
+                        Name = x.Collection.Name,
+                    }
+                })
+                .ToListAsync();
+            if (list.Count > 0)
+            {
+                collectionName = list.FirstOrDefault().Collection.Name;
+            }
+            else
+            {
+                var collection = await _dbContext.Collections
+                .AsNoTracking()
+                .Select(x => new { x.Name, x.Id })
+                .FirstOrDefaultAsync(x => x.Id == collectionId);
+
+                collectionName = collection.Name;
+            }
+            return (list, collectionName);
+        }
     }
 }
