e1047825023a8e97fc01396623f1140a94d02796
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:42:12 +0330

    Create ICardService.cs

---
+++ Services/Interfaces/ICardService.cs
@@ -0,0 +1,19 @@
+﻿using Models.ViewModels;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ICardService
+    {
+        Task AddCardAsync(CardViewModel cardViewModel);
+        Task DeleteCardAsync(CardViewModel cardViewModel);
+        Task EditCardAsync(CardViewModel cardViewModel);
+        Task<CardViewModel> GetCardAsync(long id);
+        Task<(List<CardViewModel>,string)> GetCardsAsync(long collectionId);
+        Task<CardViewModel> ReadCardAsync(long id);
+        Task<(List<CardViewModel>,string)> ReadCardsAsync(long collectionId);
+    }
+}
