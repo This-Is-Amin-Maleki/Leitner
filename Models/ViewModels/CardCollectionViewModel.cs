8cf709ec55172862fb53db9962f8ba6588f1d7e2
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:42:03 +0330

    Create CardCollectionViewModel.cs

---
+++ Models/ViewModels/CardCollectionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public record CardCollectionViewModel
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
