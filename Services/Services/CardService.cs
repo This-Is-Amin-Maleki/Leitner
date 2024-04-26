8d7ff12afec9f2e683cc5c88e62cf998c2ff3be8
Amin Maleki <AminMaleki@Live.com>
2024-04-26 17:44:03 +0330

    Create CardService

---
+++ Services/Services/CardService.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using Models.ViewModels;
+using Services.Interfaces;
+using Shared;
+using Models.DTOs;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Services.Services
+{
+    public class CardService : ICardService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CardService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
