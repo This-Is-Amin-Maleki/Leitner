b2e14fc6d1c82ba54d053582a7d52fc946628ce6
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:28:26 +0300

    Create CollectionMiniViewModel.cs

---
+++ Models/ViewModels/CollectionMiniViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SharedLeit;
+
+namespace ModelsLeit.ViewModels
+{
+    public record CollectionMiniViewModel
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public CollectionStatus? Status { get; set; }
+    }
+}
