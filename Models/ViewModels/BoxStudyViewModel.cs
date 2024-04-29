303a710e2d2d3684a1d2fed97164c92de64f924a
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:28:17 +0300

    Create BoxStudyViewModel.cs

---
+++ Models/ViewModels/BoxStudyViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelsLeit.ViewModels
+{
+    public record BoxStudyViewModel
+    {
+        public long Id { get; set; }
+        public int CardPerDay { get; set; }
+        public int LastSlot { get; set; }
+        public long LastCardId { get; set; }
+        public long CollectionId { get; set; }
+        public DateTime DateStudied { get; set; }
+    }
+}
