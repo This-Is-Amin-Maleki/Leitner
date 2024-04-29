5e48a1690f72a1a9985f0baf567cf009400f591a
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:27:56 +0300

    Create ContainerCard.cs

---
+++ Models/Entities/ContainerCard.cs
@@ -0,0 +1,23 @@
+﻿using SharedLeit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLeit.Entities
+{
+    public record ContainerCard
+    {
+        public long Id { get; set; }
+
+        //[ForeignKey("Container")]
+        public long ContainerId { get; set; }
+        public Container Container{ get; set; }
+
+        //[ForeignKey("Card")]
+        public long CardId { get; set; }
+        public Card Card { get; set; }
+    }
+}
