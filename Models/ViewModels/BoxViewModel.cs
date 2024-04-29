6aadca8c9359e7cc6079dc939c3ca6d9e00bee9d
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 21:08:57 +0300

    Create BoxViewModel.cs

---
+++ Models/ViewModels/BoxViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Models.Entities;
+
+namespace Models.ViewModels
+{
+    public record BoxViewModel
+    {
+        public long Id { get; set; }
+        public int CardPerDay { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime DateStudied { get; set; }
+        public int LastSlot { get; set; }
+        public long LastCardId { get; set; }
+        public bool Completed { get; set; }
+
+        public CollectionMiniViewModel Collection { get; set; }
+        public ICollection<Slot> Slots { get; set; }
+    }
+}
