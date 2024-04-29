35dd72c0217cc8df04e3f56cae13d90f3276a4d9
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:27:59 +0300

    Update Container.cs

--- Models/Entities/Container.cs
+++ Models/Entities/Container.cs
@@ -11,10 +11,11 @@ namespace ModelsLeit.Entities
     public record Container
     {
         public long Id { get; set; }
-        public ICollection<Card> Cards { get; set; }
+        public DateTime DateModified {  get; set; }
 
         [ForeignKey("Slot")]
         public long SlotId { get; set; }
         public Slot Slot { get; set; }
+        public ICollection<ContainerCard> ContainerCards { get; set; }
     }
 }
