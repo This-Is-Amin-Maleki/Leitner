1facc44c134c9bb1cfb325656f83bd7c9563b041
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 21:06:01 +0300

    Create BoxService.cs

---
+++ Services/Services/BoxService.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Models.DTOs;
+using Models.Entities;
+using Models.ViewModels;
+using System.Data;
+
+namespace Services.Services
+{
+    public class BoxService
+    //: IBoxService
+    {
+        private readonly ILogger<BoxService> _logger;
+
+        private readonly ApplicationDbContext _dbContext;
+        private readonly CardService _cardService;
+        public BoxService(ApplicationDbContext dbContext, CardService cardService, ILogger<BoxService> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _cardService = cardService;
+        }
+    }
+}
