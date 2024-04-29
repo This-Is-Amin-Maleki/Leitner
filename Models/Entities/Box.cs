3d9191884a70a4ffb7038672a43a8542f5e44921
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:28:05 +0300

    Update Box.cs

--- Models/Entities/Box.cs
+++ Models/Entities/Box.cs
@@ -12,16 +12,18 @@ namespace ModelsLeit.Entities
     {
         public long Id { get; set; }
 
-        public DateTime AddedForStudyDate { get; set; }
+        public DateTime DateAdded { get; set; }
 
-        public DateTime LastReviewedDate { get; set; }
+        public DateTime DateStudied { get; set; }
         /// <summary>
         /// Default value should be -2, indicating: 'Start studying from slot 4 and decrease the slot number after each study, until it reaches -2'.
         /// -1 indicating : 'It'ss tempSlot, add some new cards to fill and study mistakes'.
         /// 0-4 indicating : 'Start studying cards of slot[(2^StdySlot) - 1]'.
         /// </summary>
-        public int LastReviewedSlot { get; set; }
-        public long LastReviewedCardId { get; set; }
+        public int LastSlot { get; set; }
+        public long LastCardId { get; set; }
+        public int CardPerDay { get; set; }
+        public bool Completed { get; set; }
 
         //[ForeignKey("Collection")]
         public long CollectionId { get; set; }
