4166780e51fdc13431e984597528ded853f8e989
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-04-29 22:28:29 +0300

    Create ContainerSlotViewModel.cs

---
+++ Models/ViewModels/ContainerSlotViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelsLeit.ViewModels
+{
+    public record ContainerSlotViewModel
+    {
+        public long Id { get; set; }
+        public int Order { get; set; }
+        public long BoxId {  get; set; }
+    }
+}
