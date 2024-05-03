9b936c075690ca25dcfb5553365980c1729d6d16
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-05-03 20:57:01 +0300

    Create DataBaseExtensions.cs

---
+++ View/Extensions/DataBaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ViewLeit.Extensions
+{
+    public static class DataBaseExtensions
+    {
+        public static async void AutoMigration<T>(this IHost host) where T :DbContext
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<T>();
+            context.Database.EnsureCreated();
+        }
+    }
+}
