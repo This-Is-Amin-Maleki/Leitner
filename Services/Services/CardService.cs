08c0ddd7d966beaa64f62302574de6e0d5112c85
This-Is-Amin-Maleki <aminmaleki@dr.com>
2024-05-03 20:56:09 +0300

    Update CardService.cs

--- Services/Services/CardService.cs
+++ Services/Services/CardService.cs
@@ -6,20 +6,50 @@ using ServicesLeit.Interfaces;
 using SharedLeit;
 using System.Collections;
 using System.Collections.Generic;
+using EFCore.BulkExtensions;
+using System.Xml;
+using Microsoft.Extensions.Logging;
 
 namespace ServicesLeit.Services
 {
     public class CardService : ICardService
     {
+        private readonly ILogger<BoxService> _logger;
         private readonly ApplicationDbContext _dbContext;
-
-        public CardService(ApplicationDbContext dbContext)
+        private readonly CollectionService _collection;
+        public CardService(ApplicationDbContext dbContext, ILogger<BoxService> logger, CollectionService collection)
         {
+            _logger = logger;
             _dbContext = dbContext;
+            _collection = collection;
+        }
+        public async Task<List<CardViewModel>> ReadCardsLimitedAsync(long collectionId)
+        {//use auto mapper
+#warning if invalid card send 2 save reyurn without collection
+            string collectionName = "";
+            List<CardViewModel> list = await _dbContext.Cards
+                .AsNoTracking()
+                .Include(x => x.Collection)
+                .Where(x => x.CollectionId == collectionId)
+                .Select(x => new CardViewModel()
+                {
+                    Id = x.Id,
+                    Ask = x.Ask,
+                    HasMp3 = x.HasMp3,
+                    Collection = new CollectionMiniViewModel()
+                    {
+                        Id = x.Collection.Id,
+                        Name = x.Collection.Name,
+                        Status = x.Collection.Status,
+                    }
+                })
+                .ToListAsync();
+            return list;
         }
 
         public async Task<(List<CardViewModel>, string)> ReadCardsAsync(long collectionId)
         {//use auto mapper
+#warning if invalid card send 2 save reyurn without collection
             string collectionName = "";
             List<CardViewModel> list = await _dbContext.Cards
                 .AsNoTracking()
@@ -32,7 +62,7 @@ namespace ServicesLeit.Services
                     Answer = x.Answer,
                     Description = x.Description,
                     HasMp3 = x.HasMp3,
-                    Collection = new CardCollectionViewModel()
+                    Collection = new CollectionMiniViewModel()
                     {
                         Id = x.Collection.Id,
                         Name = x.Collection.Name,
@@ -107,9 +137,10 @@ namespace ServicesLeit.Services
                 CreateEmptyCardViewModel() :
                 MapCardToViewModel(card);
         }
-        public async Task AddCardAsync(CardViewModel cardViewModel)
+        public async Task AddCardAsync(CardViewModel model)
         {
-            var card = MapViewModelToCard(cardViewModel);
+            await _collection.CheckStatusAsync(model.Collection.Id, "Can not add any cards to the @ collection!");
+            var card = MapViewModelToCard(model);
             card.Id = 0;
 
             try
@@ -122,12 +153,41 @@ namespace ServicesLeit.Services
 
             }
         }
-        public async Task EditCardAsync(CardViewModel cardViewModel)
+        public async Task BulkAddCardsAsync(List<Card> model)
         {
-            var card = MapViewModelToCard(cardViewModel);
+            await _collection.CheckStatusAsync(model.First().CollectionId, "Can not add any cards to the @ collection!");
+            await _dbContext.BulkInsertAsync(model);
+        }
+        public async Task<int> AddCardsAsync(List<Card> model)
+        {
+            await _collection.CheckStatusAsync(model.First().CollectionId, "Can not add any cards to the @ collection!");
+            int counter = 0;
+            int done = 0;
+            try
+            {
+                foreach (Card card in model)
+                {
+                    await _dbContext.Cards.AddAsync(card);
+                    if ((++counter) % 10 is 0)
+                    {
+                        done += await _dbContext.SaveChangesAsync();
+                    }
+                }
+                done += await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return done;
+        }
+        public async Task UpdateCardAsync(CardViewModel model)
+        {
+            await _collection.CheckStatusAsync(model.Collection.Id, "Can not update any card of the @ collection!");
+            var card = MapViewModelToCard(model);
 
             var hasCard = await _dbContext.Cards
-                .AnyAsync(x => x.Id == cardViewModel.Id);
+                .AnyAsync(x => x.Id == model.Id);
 
             if (hasCard is false)
             {
@@ -144,12 +204,13 @@ namespace ServicesLeit.Services
 
             }
         }
-        public async Task DeleteCardAsync(CardViewModel cardViewModel)
+        public async Task DeleteCardAsync(CardViewModel model)
         {
-            var card = MapViewModelToCard(cardViewModel);
+            await _collection.CheckStatusAsync(model.Collection.Id, "Can not delete any card of the @ collection!");
+            var card = MapViewModelToCard(model);
 
             var hasCard = await _dbContext.Cards
-                .AnyAsync(x => x.Id == cardViewModel.Id);
+                .AnyAsync(x => x.Id == model.Id);
 
             if (hasCard is false)
             {
@@ -169,8 +230,33 @@ namespace ServicesLeit.Services
             }
         }
 
+        public async Task<List<CardViewModel>> ReadCardsAsync(long lastCardId, long collectionId, int count, int skip = 0)
+        {
+            //get new cardsArray
+            return await _dbContext.Cards
+                .AsNoTracking()
+                .Where(x => x.Id > lastCardId && x.CollectionId == collectionId)
+                .Select(x => new CardViewModel
+                {
+                    Id = x.Id,
+                    Answer = x.Answer,
+                    Ask = x.Ask,
+                    Description = x.Description,
+                    HasMp3 = x.HasMp3,
+                })
+                .Skip(skip)
+                .Take(count)
+                .ToListAsync();
+        }
+        public async Task<List<Card>> ReadCardsAsync(IEnumerable<long> cards)
+        {
+            return await _dbContext.Cards
+                .AsNoTracking()
+                .Where(x => cards.Contains(x.Id))
+                .ToListAsync();
+        }
+
         ////////////////////////////////////////////////////////
-        
         private CardViewModel CreateEmptyCardViewModel()
         {
             return new CardViewModel();
@@ -185,7 +271,7 @@ namespace ServicesLeit.Services
                 Answer = card.Answer,
                 Description = card.Description,
                 HasMp3 = card.HasMp3,
-                Collection = new CardCollectionViewModel()
+                Collection = new CollectionMiniViewModel()
                 {
                     Id = card.Collection.Id,
                     Name = card.Collection.Name,
