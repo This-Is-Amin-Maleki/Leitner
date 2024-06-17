f80f89b9caf58b70b7413db77acd7150c25c5ba2
Amin Maleki <AminMaleki@Live.com>
2024-06-17 10:51:07 +0330

    add ApplicationUser model to Entities of Model layer

---
+++ Models/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLeit.Entities
+{
+    public class ApplicationUser:IdentityUser<long>
+    {
+        public bool Active {  get; set; }
+    }
+}
