03d2f75118697b6aef92bba7df059ac68429dd16
Amin Maleki <AminMaleki@Live.com>
2024-06-17 10:51:42 +0330

    add UserRole model to Entities of Model layer

---
+++ Models/Entities/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SharedLeit;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ModelsLeit.Entities
+{
+    public class UserRole : IdentityRole<long>
+    {
+        public UserType UserType { get; set; }
+
+        public UserRole() { }
+
+        public UserRole(string roleName, UserType userType) : base(roleName)
+        {
+            UserType = userType;
+        }
+    }
+}
