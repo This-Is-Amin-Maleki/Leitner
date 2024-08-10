bded0dd374d6dd29c5fa89f3e0bd42696278e708
Amin Maleki <AminMaleki@Live.com>
2024-08-10 23:28:02 +0330

    Create ApplicationUser FluentApi in UserConfiguration

---
+++ DataAccess/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelsLeit.Entities;
+
+namespace DataAccessLeit.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(x=> x.Id);
+
+            builder.Property(e => e.Bio).HasMaxLength(500);
+
+        }
+    }
+}
