c11b3ecbbee250ce95c6d3656dfd3565e1f00eeb
Amin Maleki <AminMaleki@Live.com>
2024-08-10 23:25:58 +0330

    Update ApplicationUser.cs
    add name and bio

--- Models/Entities/ApplicationUser.cs
+++ Models/Entities/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,10 @@ namespace ModelsLeit.Entities
     public class ApplicationUser:IdentityUser<long>
     {
         public bool Active {  get; set; }
+        [Required]
+        [Length(5,100)]
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
     }
 }
