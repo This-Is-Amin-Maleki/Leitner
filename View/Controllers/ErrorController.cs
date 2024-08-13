3fc8f63f79481ba9f3b9cf6260e8c4fdc7200770
Amin Maleki <AminMaleki@Live.com>
2024-08-13 15:08:12 +0330

    Update Error Controller and View

--- View/Controllers/ErrorController.cs
+++ View/Controllers/ErrorController.cs
@@ -1,24 +1,43 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ModelsLeit.DTOs;
 using ModelsLeit.ViewModels;
+using ServicesLeit.Interfaces;
+using ServicesLeit.Services;
 using System.Diagnostics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ViewLeit.Controllers
 {
     public class ErrorController : Controller
     {
         private readonly ILogger<ErrorController> _logger;
+        private readonly FileService _fileService;
 
-        public ErrorController(ILogger<ErrorController> logger)
+        public ErrorController(ILogger<ErrorController> logger, FileService fileService)
         {
             _logger = logger;
+            _fileService = fileService;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(new ErrorDto { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(HttpContext.Response.StatusCode > 400)
+            {
+                var error = new ErrorDto { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+                if (error.ShowRequestId)
+                {
+                    var data = $"{DateTime.Now},{error.RequestId}";
+                    await _fileService.WriteToFileAsync("Error.log", data);
+                }
+                return View("Index", error);
+            }
+            return RedirectToAction("Index", "Page");
         }
+
     }
 }
