c8499eb66a8b5c00f65e2f9f8ad327bbcbb198e5
Amin Maleki <AminMaleki@Live.com>
2024-08-13 15:11:31 +0330

    Add Page Controller, View for home page

--- View/Controllers/PageController.cs
+++ View/Controllers/PageController.cs
@@ -6,41 +6,42 @@ using ViewLeit.Extensions;
 using ServicesLeit.Services;
 using ModelsLeit.ViewModels.User;
 using Microsoft.AspNetCore.Authorization;
+using ModelsLeit.DTOs.Collection;
 
 namespace View.Controllers
 {
     [AllowAnonymous]
     public class PageController : Controller
     {
-        /*
+        private readonly CollectionService _collectionService;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IConfiguration _configuration;
-        private readonly IEmailSender<ApplicationUser> _emailSender;
-        private readonly IHttpContextAccessor _httpContext;
-        private readonly RoleManager<UserRole> _roleManager;
-        private readonly UserService _userService;
-        private readonly ApplicationDbContext _db;  //need before repository
-        private readonly ILogger<PageController> _logger;
-        private readonly IConfiguration _configuration;
-        private readonly IHttpContextAccessor _httpContext;
-        private readonly PageRepository _pageRepository;
-        private readonly LanguageRepository _languageRepository;
-        */
-        private readonly CollectionService _collectionService;
 
-        public PageController(CollectionService collectionService)
+        public PageController(
+            CollectionService collectionService,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _collectionService = collectionService;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IActionResult> Index()
         {
             HomePageViewModel model = new();
-            var collections = await _collectionService.ReadPublishedCollectionsAsync();
-            model.Collections = collections;
-
+            model.Collections = await ReadCollections();
             return View(model);
+        }
+
+        private async Task<IEnumerable<CollectionDto>> ReadCollections()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = long.Parse(_userManager.GetUserId(User)!);
+                return await _collectionService.ReadUnusedPublishedCollectionsAsync(user);
+            }
+            return await _collectionService.ReadPublishedCollectionsAsync();
         }
     }
 }
