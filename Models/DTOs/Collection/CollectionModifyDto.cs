7308e72da53fc2d865168c52083a01cf8f48ba1d
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:31 +0330

    Create CollectionModifyDto.cs

---
+++ Models/DTOs/Collection/CollectionModifyDto.cs
@@ -0,0 +1,13 @@
+﻿using SharedLeit;
+
+namespace ModelsLeit.DTOs.Collection
+{
+    public record CollectionModifyDto
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string? Name { get; set; }
+        public string Description { get; set; }
+        public CollectionStatus Status { get; set; }
+    }
+}
