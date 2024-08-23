a8725125c43e5a57ff7d43e8984ae11e6133b4f0
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:10 +0330

    Create CardMiniUnlimitedDto.cs

---
+++ Models/DTOs/Card/CardMiniUnlimitedDto.cs
@@ -0,0 +1,15 @@
+﻿using ModelsLeit.DTOs.Collection;
+using SharedLeit;
+
+namespace ModelsLeit.DTOs.Card
+{
+    public record CardMiniUnlimitedDto
+    {
+        public long Id { get; set; }
+        public string Ask { get; set; }
+        public string Answer { get; set; }
+        public CardStatus Status { get; set; }
+        public bool HasMp3 { get; set; }
+        public CollectionMiniDto Collection { get; set; }
+    }
+}
