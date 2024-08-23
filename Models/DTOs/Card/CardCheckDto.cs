c2e94560d656fa509f66fe745e6b67bf54c77314
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:03 +0330

    Create CardCheckDto.cs

---
+++ Models/DTOs/Card/CardCheckDto.cs
@@ -0,0 +1,14 @@
+﻿using SharedLeit;
+
+namespace ModelsLeit.DTOs.Card
+{
+    public record CardCheckDto
+    {
+        public long Id { get; set; }
+        public string Ask { get; set; }
+        public string Answer { get; set; }
+        public string Description { get; set; }
+        public bool HasMp3 { get; set; }
+        public CardStatus Status { get; set; }
+    }
+}
