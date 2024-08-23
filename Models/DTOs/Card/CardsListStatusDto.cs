062cbdb6611a051baf0cab9a8c9a9038359328e5
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:14 +0330

    Create CardsListStatusDto.cs

---
+++ Models/DTOs/Card/CardsListStatusDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelsLeit.DTOs.Card
+{
+    public record CardsListStatusDto
+    {
+        public long Id { get; set; }
+        public string? CollectionName { get; set; }
+        public List<CardCheckDto> Submitted { get; set; }
+        public List<CardCheckDto> Approved { get; set; }
+        public List<CardCheckDto> Blocked { get; set; }
+        public List<CardCheckDto> Rejected { get; set; }        
+
+    }
+}
