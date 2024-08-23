dd77d222ebdd273872eda0219e6727e84bd6f2da
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:17 +0330

    Update CollectionCreationDto.cs

--- Models/DTOs/Collection/CollectionCreationDto.cs
+++ Models/DTOs/Collection/CollectionCreationDto.cs
@@ -4,10 +4,8 @@ namespace ModelsLeit.DTOs.Collection
 {
     public record CollectionCreationDto
     {
-        public long Id { get; set; }
         public long UserId { get; set; }
         public string? Name { get; set; }
         public string Description { get; set; }
-        public CollectionStatus? Status { get; set; }
     }
 }
