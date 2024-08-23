9a763e7ffea8872584dfea39f53201dc9fb6ab78
Amin Maleki <AminMaleki@Live.com>
2024-08-23 19:58:09 +0330

    Change UserRole to IdentityRole
    - Update DataAccess
    - Remove UserRole Entity

--- DataAccess/Context/ApplicationDbContext.cs
+++ DataAccess/Context/ApplicationDbContext.cs
@@ -1,21 +1,16 @@
 ﻿using DataAccessLeit.Data.Configurations;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Options;
 using ModelsLeit.Entities;
-using SharedLeit;
-using System.Drawing;
-using System.Xml.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace DataAccessLeit.Context
 {
-    public class ApplicationDbContext: IdentityDbContext<ApplicationUser, UserRole, long>
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser,IdentityRole<long>,long>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-        { }
+        : base(options){}
+        
         public DbSet<Collection> Collections { get; set; }
         public DbSet<Card> Cards { get; set; }
         public DbSet<Box> Boxes { get; set; }
@@ -23,19 +18,12 @@ namespace DataAccessLeit.Context
         public DbSet<Container> Containers { get; set; }
         public DbSet<ContainerCard> ContainerCards { get; set; }
 
-        public object AsNoTracking()
-        {
-            throw new NotImplementedException();
-        }
-
-        //public DbSet<ApplicationUser> Users { get; set; }
-
-        /*
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Data Source=31.25.90.164\\sqlserver2019;Database = aminmal1_leiti;User ID=aminmal1_leitUsr;Password=Alba2374;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"
-                /*"Database = Leitner;Data Source=ARTEMISIA;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"* /);
-        }*/
+            options.UseSqlServer(
+                "Database = Leitner;Data Source=ARTEMISIA;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"
+                 );
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -47,8 +35,6 @@ namespace DataAccessLeit.Context
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(BoxConfiguration).Assembly);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContainerConfiguration).Assembly);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContainerCardConfiguration).Assembly);
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
