879414be1bc3f752e363d351ff55b514495febe0
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:28 +0330

    Create CollectionListDto.cs

---
+++ Models/DTOs/Collection/CollectionListDto.cs
@@ -0,0 +1,17 @@
+﻿using ModelsLeit.DTOs.User;
+using SharedLeit;
+
+namespace ModelsLeit.DTOs.Collection
+{
+    public record CollectionListDto
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public CollectionStatus Status { get; set; }
+        public UserMiniDto User { get; set; }
+
+        public long CardsQ { get; set; }
+        public long BoxCount { get; set; }
+    }
+}
