909a742c87aee2842237631d0bdebfa7008ede26
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:07:05 +0330

    Create CardCheckedResultDto.cs

---
+++ Models/DTOs/Card/CardCheckedResultDto.cs
@@ -0,0 +1,10 @@
+﻿using ModelsLeit.DTOs.Collection;
+
+namespace ModelsLeit.DTOs.Card
+{
+    public record CardCheckedResultDto
+    {
+        public CollectionMiniDto Collection { get; set; }
+        public long CheckedCards { get; set; }
+    }
+}
