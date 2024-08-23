49ea585d11c81f8931188e951f7b3e554f15d69d
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:06:53 +0330

    Create UserMiniDto.cs

---
+++ Models/DTOs/Account/UserMiniDto.cs
@@ -0,0 +1,11 @@
+﻿using SharedLeit;
+
+namespace ModelsLeit.DTOs.User
+{
+    public record UserMiniDto
+
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
