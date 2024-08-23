a628f085f72fd88745a867b710ea88902ee10410
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:10:39 +0330

    Add Migration
    Add Migration useIdentityRole+AddUserIdToBox.Card.Collection+AddStatusToCard

---
+++ DataAccess/Migrations/20240823154347_useIdentityRole+AddUserIdToBox.Card.Collection+AddStatusToCard.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLeit.Migrations
+{
+    /// <inheritdoc />
+    public partial class useIdentityRoleAddUserIdToBoxCardCollectionAddStatusToCard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                schema: "dbo",
+                table: "AspNetRoles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                schema: "dbo",
+                table: "Cards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "dbo",
+                table: "Cards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                schema: "dbo",
+                table: "AspNetRoles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
