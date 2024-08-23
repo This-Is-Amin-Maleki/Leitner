dfc5a0036a920e473c4aa6b75544c825b823d922
Amin Maleki <AminMaleki@Live.com>
2024-08-23 20:51:49 +0330

    Update DataBaseExtensions.cs

--- View/Extensions/DataBaseExtensions.cs
+++ View/Extensions/DataBaseExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using ModelsLeit.Entities;
 using SharedLeit;
+using System;
 using System.Data;
 
 namespace ViewLeit.Extensions
@@ -65,19 +66,15 @@ namespace ViewLeit.Extensions
         }
         private static async Task SeedRolesAsync(IServiceProvider services)
         {
-            var roleManager = services.GetRequiredService<RoleManager<UserRole>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole<long>>>();
 
             //check exist any roles? Add all roles
             var countRoles = await roleManager.Roles.CountAsync();
             if (countRoles is 0)
             {
-                foreach (var role in Enum.GetValues(typeof(UserType)))
+                foreach (var role in Enum.GetNames(typeof(UserType)))// Enum.GetValues(typeof(UserType)))
                 {
-                    UserRole userRole = new()
-                    {
-                        Name = role.ToString(),
-                        UserType = (UserType)role,
-                    };
+                    IdentityRole<long> userRole = new (role);
                     //add admin role
                     await roleManager.CreateAsync(userRole);
                 }
