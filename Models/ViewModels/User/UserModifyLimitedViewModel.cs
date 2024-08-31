d9e09e4f04e363a60126b274ee4cc00b05d565d6
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:04:10 +0330

    Update UserModifyLimitedViewModel.cs

--- Models/ViewModels/User/UserModifyLimitedViewModel.cs
+++ Models/ViewModels/User/UserModifyLimitedViewModel.cs
@@ -8,13 +8,36 @@ namespace ModelsLeit.ViewModels.User
         [EmailAddress]
         public string? Email { get; set; }
         public string? Phone { get; set; }
+        [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "{0} - {1} - {2} Characters")]
         public string? Password { get; set; }
+        [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "{0} - {1} - {2} Characters")]
-        public string? NewPassword { get; set; }
+        public string? NewPassword { get; set; }    [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} - {1} - {2} Characters")]
         public string? Name { get; set; }
         [StringLength(255, MinimumLength = 0, ErrorMessage = "{0} - {1} - {2} Characters")]
         public string? Bio { get; set; }
+    }
+    public record UserModifyProfileLimitedViewModel
+    {
+        public long Id { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} - {1} - {2} Characters")]
+        public string? Name { get; set; }
+        [StringLength(255, MinimumLength = 0, ErrorMessage = "{0} - {1} - {2} Characters")]
+        public string? Bio { get; set; }
+    }
+    public record UserChangePasswordLimitedViewModel
+    {
+        public long Id { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "{0} - {1} - {2} Characters")]
+        public string? Password { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "{0} - {1} - {2} Characters")]
+        public string? NewPassword { get; set; }
     }
 }
