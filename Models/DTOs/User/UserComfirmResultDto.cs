51dd28298d434e5bb2a70934712fef64a625aca6
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:04:44 +0330

    Create UserComfirmResultDto.cs

---
+++ Models/DTOs/User/UserComfirmResultDto.cs
@@ -0,0 +1,12 @@
+﻿using ModelsLeit.ViewModels.User;
+using SharedLeit;
+
+namespace ModelsLeit.DTOs.User
+{
+    public record UserComfirmResultDto
+    {
+        public ComfirmationStatus Status { get; set; }
+        public List<string>? Errors { get; set; }
+        public UserResetPasswordViewModel? ResetPassword { get; set; }
+    }
+}
