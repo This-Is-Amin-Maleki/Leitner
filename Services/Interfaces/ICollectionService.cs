77b71b743dac3891ef86c50313f8696af1c3f4c7
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:00:48 +0330

    Update ICollectionService.cs

--- Services/Interfaces/ICollectionService.cs
+++ Services/Interfaces/ICollectionService.cs
@@ -6,11 +6,11 @@ namespace ServicesLeit.Interfaces
     public interface ICollectionService
     {
         Task AddCollectionAsync(CollectionCreationDto collectionViewModel);
-        Task<BoxAddDto?> CheckExistCollectionAsync(long id);
+        Task<BoxAddDto?> CheckExistCollectionAsync(long id, long userId);
         Task CheckStatusAsync(long id, string message);
         Task DeleteCollectionAsync(CollectionDto collectionViewModel);
         Task DeleteCollectionLimitedAsync(CollectionModifyDto model);
-        Task EditCollectionAsync(CollectionDto model);
+        Task EditCollectionAsync(CollectionModifyDto model);
         Task EditCollectionLimitedAsync(CollectionModifyDto model);
         Task<List<CollectionListDto>> ReadAllAsync();
         Task<List<CollectionListDto>> ReadAllAsync(long userId);
