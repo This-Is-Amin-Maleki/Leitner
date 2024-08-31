29ea99b0b844f1c26766ca8711f3c8c0685de23c
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:04:27 +0330

    Create UserLoginCheckDto.cs

---
+++ Models/DTOs/User/UserLoginCheckDto.cs
@@ -0,0 +1,10 @@
+﻿using ModelsLeit.Entities;
+
+namespace ModelsLeit.DTOs.User
+{
+    public record UserLoginCheckDto
+    {
+        public ApplicationUser User { get; set; }
+        public string Password { get; set; }
+    }
+}
