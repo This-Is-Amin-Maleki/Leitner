158dc22d10b728583391e871b9dcca9cf081864e
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:04:39 +0330

    Create UserComfirmTokenDto.cs

---
+++ Models/DTOs/User/UserComfirmTokenDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelsLeit.DTOs.User
+{
+    public record UserComfirmTokenDto
+    {
+        public string Identifier { get; set; }
+        public string Token { get; set; }
+        public bool IsPassResetRequest { get; set; }
+    }
+}
