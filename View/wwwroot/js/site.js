6fad034e29e0c3fa77bdfd4047cb71c533bb33dd
Amin Maleki <AminMaleki@Live.com>
2024-08-31 20:07:12 +0330

    update Layout

--- View/wwwroot/js/site.js
+++ View/wwwroot/js/site.js
@@ -1,4 +1,43 @@
 ﻿$(document).ready(function () {
+    var changeMode = function (e) {
+        $("html").addClass("change");
+        var mode = $("html").attr("data-bs-theme");
+        var evalue = true;
+        if (mode == "dark") {
+            mode = "light";
+            evalue = false;
+        } else {
+            mode = "dark";
+            evalue = true;
+        }
+        $("html").attr("data-bs-theme", mode);
+        localStorage.setItem("DarkMode",mode);
+        $(e).prop('checked', evalue);
+        setTimeout(function () {
+            $("html").removeClass("change");
+        }, 1100);
+        
+    }
+    var systemMode = function () {
+        var mode = localStorage.getItem("DarkMode");
+        var evalue = true;
+        if (mode == null) {
+            mode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        }
+        if (mode == "dark") {
+            mode = 'dark';
+            evalue = true;
+        } else {
+            mode = 'light';
+            evalue = false;
+        }
+
+        $("html").attr("data-bs-theme", mode);
+        $("#modeChanger").prop('checked', evalue).click(function () {
+            changeMode(this);
+        });
+    }
+    systemMode();
     $('a[href]:not([href^="#"])').click(function (event) {
         event.preventDefault();
         var target = $(this).attr('target');
