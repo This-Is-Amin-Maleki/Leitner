542bfd26471f1d21816356d5901901d9cf8c3205
Amin Maleki <AminMaleki@Live.com>
2024-09-03 23:32:27 +0330

    Update card checking method in AdminCardController
    - Optimized card checking method to process cards individually instead of all at once.
    - Added capability to check cards based on their status.

--- View/Controllers/AdminCardController.cs
+++ View/Controllers/AdminCardController.cs
@@ -5,10 +5,6 @@ using ServicesLeit.Services;
 using SharedLeit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using ModelsLeit.DTOs.Container;
-using Newtonsoft.Json;
-using ServicesLeit.Interfaces;
-using System.Data;
 using System.Runtime.InteropServices;
 
 namespace ViewLeit.Controllers
@@ -83,46 +79,7 @@ namespace ViewLeit.Controllers
                  return View(model);
              }
         }
-        /*
-        // POST: CardController/Delete
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Delete(CardDto model)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError("XX", "Not Valid!");
-                return View(model);
-            }
 
-            try
-            {
-                await _cardService.DeleteCardAsync(model);
-                return RedirectToAction(nameof(Index), new { id = model.Collection.Id });
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("xx", ex.Message);
-                return View(model);
-            }
-        }*/
-        public async Task<ActionResult> CheckAll(long id)
-        {
-            try
-            {
-                var model = await _cardService.ReadCardsByStatusAsync(id);
-                if (model is null && model.Submitted.Count is 0)
-                {
-                    ViewData["er"] = "No cards left to check";
-                    return RedirectToAction(nameof(Index), new { id });
-                }
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                return RedirectToAction(nameof(Index), new { id });
-            }
-        }
         public async Task<ActionResult> TickAll(long id)
         {
             try
@@ -136,33 +93,84 @@ namespace ViewLeit.Controllers
             return RedirectToAction(nameof(Index), new { id });
         }
 
-        public async Task<ActionResult> Check(long id)
+        [HttpGet]
+        public async Task<ActionResult> Check(long id, CardStatus? status, int skip = 0)
         {
+            CardCheckDto output;
             try
             {
-                var model = await _cardService.ReadCardsByStatusAsync(id);
-                if (model is null && model.Submitted.Count is 0)
-                {
-                    ViewData["er"] = "No cards left to check";
-                    return RedirectToAction(nameof(Index), new { id });
-                }
-                return View(model);
+                output = await _cardService.ReadCardCheck(id, status, skip);
+                output.Skip = skip + 1;
             }
             catch (Exception ex)
             {
-                return RedirectToAction(nameof(Index), new {id});
+                TempData["Error"] = ex.Message;
+                return RedirectToAction(nameof(Index), new { id });
             }
+            return View(output);
         }
 
-        // POST: BoxController/Add
         [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public async Task<ActionResult> Check(CardsArrayStatusDto model)
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CheckUp(CardCheckStatusDto model)
         {
-            CardCheckedResultDto cardsUpdated = await _cardService.BulkUpdateCardsStatusAsync(model);
-            ViewData["Collection"] = cardsUpdated.Collection.Name;
-            ViewData["CardsCount"] = cardsUpdated.CheckedCards;
-            return RedirectToAction(nameof(Index), new {id = model.Id});
+            string content;
+            try
+            {
+                var output = await _cardService.UpdateStatusAndReadNextCardCheck(model);
+                if (output.Id is not 0)
+                {
+                    return Json(output);
+                }
+
+                content = Url.Action(
+                nameof(Index),
+                new
+                {
+                    id = model.CollectionId,
+                    state = output.Status
+                })!;
+                return Json(content);
+
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+            }
+
+            content = Url.Action(
+            nameof(Index),
+            new
+            {
+                id = model.CollectionId,
+            })!;
+            return Json(content);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CheckNext(CardCheckStatusDto model)
+        {
+            CardCheckDto output;
+            try
+            {
+                output = await _cardService.ReadCardCheck(model.Id, model.Status, model.Skip);
+                output.Skip = model.Skip + 1;
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+
+                var content = Url.Action(
+                nameof(Index),
+                new
+                {
+                    id = model.CollectionId,
+                })!;
+                return Json(content);
+            }
+            return Json(output);
+        }
+
     }
 }
