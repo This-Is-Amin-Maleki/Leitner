6e2b8d4f04917fca4cde8e94475fa2098c0039bf
Amin Maleki <AminMaleki@Live.com>
2024-09-04 22:50:13 +0330

    Add BoxCount property to CollectionModifyDto
    - Introduced `BoxCount` property to `CollectionModifyDto` to store the count of boxes.

--- Models/DTOs/Collection/CollectionModifyDto.cs
+++ Models/DTOs/Collection/CollectionModifyDto.cs
@@ -6,6 +6,7 @@ namespace ModelsLeit.DTOs.Collection
     {
         public long Id { get; set; }
         public long UserId { get; set; }
+        public long BoxCount { get; set; }
         public string? Name { get; set; }
         public string Description { get; set; }
         public CollectionStatus Status { get; set; }
