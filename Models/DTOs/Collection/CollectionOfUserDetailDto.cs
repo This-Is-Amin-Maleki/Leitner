4abd5525255bc914cd95617e0927bbb7eebd6d0d
Amin Maleki <AminMaleki@Live.com>
2024-09-14 22:36:07 +0330

    Update CollectionOfUserDetailDto to allow nullable Boxes array

--- Models/DTOs/Collection/CollectionOfUserDetailDto.cs
+++ Models/DTOs/Collection/CollectionOfUserDetailDto.cs
@@ -4,7 +4,7 @@ namespace ModelsLeit.DTOs.Collection
 {
     public record CollectionOfUserDetailDto
     {
-        public long[] Boxes { get; set; }
+        public long[]? Boxes { get; set; }
         public string? Bio { get; set; }
         public List<CollectionShowDto> Collections { get; set; }
     }
