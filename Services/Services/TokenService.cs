a9316518ee30504e2b221aca711b4f69333b176b
Amin Maleki <AminMaleki@Live.com>
2024-09-23 22:57:41 +0330

    Create TokenService.cs

---
+++ Services/Services/TokenService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ModelsLeit.Entities;
+using ServicesLeit.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ServicesLeit.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+    }
+}
