9a787f43e88823665155b50ff733d09b1d1f2f27
Amin Maleki <AminMaleki@Live.com>
2024-09-24 11:40:13 +0330

    Inject necessary dependencies into AccountController

--- APILeit/Controllers/Controllers/AccountController.cs
+++ APILeit/Controllers/Controllers/AccountController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.Entities;
+using Microsoft.AspNetCore.Identity;
+using ServicesLeit.Services;
+using ModelsLeit.ViewModels.User;
+
 namespace APILeit.Controllers
 {
 
@@ -7,5 +12,23 @@ namespace APILeit.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        //private readonly ApplicationDbContext _db;
+        private readonly ILogger<AccountController> _logger;
+        //private readonly int _pageSize;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserService _userService;
+        // ApplicationDbContext db,
+
+        public AccountController(
+            ILogger<AccountController> logger,
+            // IConfiguration configuration,
+            UserManager<ApplicationUser> userManager,
+          UserService userService)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            //_pageSize = configuration.GetValue<int>("Configuration:ItemsInList");
+            _userService = userService;
+        }
     }
 }
