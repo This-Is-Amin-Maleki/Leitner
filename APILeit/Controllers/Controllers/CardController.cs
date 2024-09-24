354467c949b1dbd09a2741b7fca1c144ad768d2c
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:44:46 +0330

    Add Get method to CardController

--- APILeit/Controllers/Controllers/CardController.cs
+++ APILeit/Controllers/Controllers/CardController.cs
@@ -37,5 +37,15 @@ namespace APILeit.Controllers
             List<CardMiniDto> model = await _cardService.ReadCardsLimitedAsync(id, userId);
             return Ok(model);
         }
+
+        // GET: CardController/Details/5
+        [HttpGet]
+        [Route("api/[controller]/[action]/{id}")]
+        public async Task<ActionResult> Get(long id)
+        {
+            var userId = long.Parse(_userManager.GetUserId(User)!);
+            var model = await _cardService.ReadCardLimitedAsync(id, userId);
+            return Ok(model);
+        }
     }
 }
