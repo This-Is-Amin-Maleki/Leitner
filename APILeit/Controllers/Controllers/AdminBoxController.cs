939ac67b6554ad0bdcb4ae4368c69a01f0dcf5f6
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:07:14 +0330

    Initialize AdminBoxController in APILeit layer and Inject necessary dependencies

---
+++ APILeit/Controllers/Controllers/AdminBoxController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.DTOs.Box;
+using ServicesLeit.Services;
+using Microsoft.AspNetCore.Identity;
+using ModelsLeit.Entities;
+using Microsoft.AspNetCore.Authorization;
+using SharedLeit;
+
+namespace APILeit.Controllers
+{
+    [Authorize(Roles = nameof(UserType.Admin))]
+    [ApiController]
+    public class AdminBoxController : ControllerBase
+    {
+        private readonly BoxService _boxService;
+        private readonly CollectionService _collectionService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminBoxController(
+            BoxService boxService,
+            CollectionService collectionService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _boxService = boxService;
+            _collectionService = collectionService;
+            _userManager = userManager;
+        }
+    }
+}
