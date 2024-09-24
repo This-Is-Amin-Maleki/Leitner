8925d8b16740ac3d4bb3fdafae023242be873915
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:38:02 +0330

    Initialize BoxController in APILeit layer and Inject necessary dependencies

---
+++ APILeit/Controllers/Controllers/BoxController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.DTOs.Card;
+using ModelsLeit.DTOs.Box;
+using ServicesLeit.Services;
+using ModelsLeit.DTOs.Container;
+using Microsoft.AspNetCore.Identity;
+using ModelsLeit.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APILeit.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class BoxController : ControllerBase
+    {
+        private readonly BoxService _boxService;
+        private readonly CollectionService _collectionService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BoxController(
+            BoxService boxService,
+            CollectionService collectionService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _boxService = boxService;
+            _collectionService = collectionService;
+            _userManager = userManager;
+        }
+    }
+}
