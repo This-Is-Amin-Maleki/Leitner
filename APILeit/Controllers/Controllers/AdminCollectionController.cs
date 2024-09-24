b316fcb82bb86124b914a7e5591016c6b253593f
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:27:24 +0330

    Initialize AdminCollectionController in APILeit layer and Inject necessary dependencies

---
+++ APILeit/Controllers/Controllers/AdminCollectionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.DTOs.Collection;
+using ServicesLeit.Services;
+using SharedLeit;
+
+namespace APILeit.Controllers
+{
+    [Authorize(Roles = nameof(UserType.Admin))]
+    [ApiController]
+    public class AdminCollectionController : ControllerBase
+    {
+        private readonly CollectionService _collectionService;
+        public AdminCollectionController(
+            CollectionService collectionService)
+        {
+            _collectionService = collectionService;
+        }
+    }
+}
