a9d7fb4f2e1405cc08ef1a2e7891aa48d97c0da2
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:08:47 +0330

    Initialize AdminCardController in APILeit layer and Inject necessary dependencies

---
+++ APILeit/Controllers/Controllers/AdminCardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.DTOs.Card;
+using ModelsLeit.Entities;
+using ServicesLeit.Services;
+using SharedLeit;
+using Microsoft.AspNetCore.Identity;
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APILeit.Controllers
+{
+    [Authorize(Roles = nameof(UserType.Admin))]
+    [ApiController]
+    public class AdminCardController: ControllerBase
+    {
+        private readonly CardService _cardService;
+        private readonly CollectionService _collectionService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminCardController(
+            CardService cardService,
+            CollectionService collectionService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _cardService = cardService;
+            _collectionService = collectionService;
+            _userManager = userManager;
+        }
+    }
+}
