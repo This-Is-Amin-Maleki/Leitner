eca4df674d273e649214256ce72d459f857bfdc9
Amin Maleki <AminMaleki@Live.com>
2024-09-24 13:00:05 +0330

    Configure Authentication and DbContext in Program.cs

--- APILeit/Program.cs
+++ APILeit/Program.cs
@@ -66,6 +66,30 @@ namespace APILeit
                 }
                 });
             });
+
+
+            //builder.Services.AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<Program>());
+
+
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Identity and Authentication configuration
+            builder.Services
+                .AddIdentity<ApplicationUser, IdentityRole<long>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.Configure<PasswordHasherOptions>(option =>
+            {
+                option.IterationCount = 12000;
+            });
+
             // Configure services and DI
             builder.Services.AddScoped<ITokenService, TokenService>();
             builder.Services.AddScoped<CollectionService>();
@@ -82,16 +106,94 @@ namespace APILeit
                 return new NotificationService(smtpServiceDto);
             });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+            // JWT Configuration
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {/*
+                options.AccessDeniedPath = "/";
+                options.Cookie.Name = "Leitner";
+                options.LoginPath = "/";
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                // ReturnUrlParameter requires 
+                //using Microsoft.AspNetCore.Authentication.Cookies;
+                
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                options.SlidingExpiration = true;
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;/*
+            });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {*/
+              
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return Task.CompletedTask;
+                };
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return Task.CompletedTask;
+                };
+            });
+            /*
+            // Force Identity's security stamp to be validated every hour.
+            builder.Services.Configure<SecurityStampValidatorOptions>(options =>
+                               options.ValidationInterval = TimeSpan.FromHours(1));
+    */
+            builder.Services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedEmail = false;
+
+                    // Default Lockout settings.
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(1);
+                    //options.Lockout.MaxFailedAccessAttempts = 5;
+                    //options.Lockout.AllowedForNewUsers = true;
+
+                    // Default Password settings.
+                    //options.Password.RequireDigit = true;
+                    //options.Password.RequireLowercase = true;
+                    //options.Password.RequireNonAlphanumeric = true;
+                    //options.Password.RequireUppercase = true;
+                    //options.Password.RequiredLength = 6;
+                    //options.Password.RequiredUniqueChars = 1;
+
+                    // Default SignIn settings.
+                    //options.SignIn.RequireConfirmedEmail = false;
+                    //options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                    // Default Register settings.
+                    //options.User.AllowedUserNameCharacters =
+                    //"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                    //options.User.RequireUniqueEmail = false;
+
+                }
+            );
+            //token validation duration
+            builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromDays(1));
+
+
             var app = builder.Build();
 
             // Enable Swagger in Development
