a5a25324844be5a3398712c86162514f9b360dbc
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:00:34 +0330

    Create LoginApiReposponseDto.cs

---
+++ Models/DTOs/User/LoginApiReposponseDto.cs
@@ -0,0 +1,10 @@
+﻿using SharedLeit;
+
+namespace ModelsLeit.DTOs.User
+{
+    public record LoginApiReposponseDto
+    {
+        public string JwtToken { get; set; }
+        public LoginResult Result { get; set; }
+    }
+}
