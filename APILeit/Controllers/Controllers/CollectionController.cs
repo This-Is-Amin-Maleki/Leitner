ee23e7a4ca898634eb86614d5e800663ef9c20a4
Amin Maleki <AminMaleki@Live.com>
2024-09-24 12:47:12 +0330

    Initialize CollectionController in APILeit layer and Inject necessary dependencies

---
+++ APILeit/Controllers/Controllers/CollectionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsLeit.DTOs.Collection;
+using ServicesLeit.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ModelsLeit.Entities;
+
+namespace APILeit.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class CollectionController: ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly CollectionService _collectionService;
+        public CollectionController(
+            UserManager<ApplicationUser> userManager,
+            CollectionService collectionService)
+        {
+            _userManager = userManager;
+            _collectionService = collectionService;
+        }
+    }
+}
